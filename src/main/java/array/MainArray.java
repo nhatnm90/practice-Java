67256c836988e3647a555566ac351354ddef7c8e
nhatnguyen-sm <nhat.minh.nguyen@mediastep.com>
2021-06-02 01:29:35 +0700

    Answer for array practice and add more practice

--- src/main/java/array/MainArray.java
+++ src/main/java/array/MainArray.java
@@ -27,7 +27,18 @@ public class MainArray {
         System.out.println("The number of first array: " + firstArray.length);
 
         /*
-        * Print all the element of array with the comma as separator and end with the dot.
+        * PRACTICE: Print all the element of array with the comma as separator and end with the dot.
+        */
+
+        System.out.println();
+
+        System.out.println("C1: The list element of array by flattening a complex array to a simple array: ");
+
+        /*
+        Instead of looping through the array of array, we can flat it to a simple array
+        array = [[a, b, c], [d, e, f]];
+        ->
+        flattenArray = [a, b, c, d, e, f];
         */
 
         List<Integer> flattenArray = new ArrayList<>();
@@ -37,7 +48,8 @@ public class MainArray {
             }
         }
 
-        System.out.println("The list element of array by FOR: ");
+        // using FOR to check the last index of array and add the suitable signal (comma or dor)
+
         for (int i = 0; i < flattenArray.size(); i++) {
             System.out.print(flattenArray.get(i));
             if (i == flattenArray.size() - 1) {
@@ -47,7 +59,28 @@ public class MainArray {
             }
         }
 
-        System.out.println("C1: The list element of array by FOREACH: ");
+        // using FOREACH need to create a variable to detect the last element instead of using index of FOR
+        int index = 0;
+        for (int element: flattenArray) {
+            System.out.print(element);
+            if (index == flattenArray.size() - 1) {
+                System.out.println(". ");
+            } else {
+                System.out.print(", ");
+            }
+            index += 1;
+        }
+
+        System.out.println();
+
+        /*
+        Looping through the Array of array, within each time, looping through the array to print the element
+        Using 2 variables due to using 2 loop:
+        - 1 variable to detect last array in array of array (countArray)
+        - 1 variable to detect last element in last array and print the dot to end (countElement)
+        */
+
+        System.out.println("C2: The list element of array by looping double array ");
         int countArray = 0;
         for (int[] arr: joinedArray) {
             int countElement = 0;
@@ -63,7 +96,9 @@ public class MainArray {
             countArray += 1;
         }
 
-        System.out.println("C2: The list element of array by FOREACH: ");
+        System.out.println();
+
+        System.out.println("C3: The list element of array by looping double array and add the | at the end of each array");
         int countArray1 = 0;
         for (int[] arr: joinedArray) {
             int countElement = 0;
@@ -81,6 +116,15 @@ public class MainArray {
             }
             countArray1 += 1;
         }
+
+        /*
+        PRACTICE: if you really want to do it
+        1. Print the array with the ascending sort
+        2. Print the array without duplicate element
+        3. Get the total of the array of practice 2
+        4. From the list of practice 1, create an array of array with each child array has 2 element with ascending sort
+        Ex: array = [0, 1, 2, 4, 5, 8, 9, 10] -> resultArray = [[0, 1], [2, 4], [5, 8], [9, 10]]
+        */
 
     }
 
