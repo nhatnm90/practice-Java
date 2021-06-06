9059d30aa10cb217780a71ccb69a76ef3cf90393
nhatnguyen-sm <nhat.minh.nguyen@mediastep.com>
2021-06-06 11:59:45 +0700

    Add the explanation and some answer for the ArrayList vs Array

--- src/main/java/array/MainArray.java
+++ src/main/java/array/MainArray.java
@@ -1,6 +1,8 @@
 package array;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -22,24 +24,22 @@ public class MainArray {
         joinedArray.add(secondArray);
         joinedArray.add(thirdArray);
 
+        /* START: COMMENT OUT TO DO THE EXERCISES: you can un-comment this quote to run the example code (from START to END)
+
         System.out.println("The number of array: " + joinedArray.size());
 
         System.out.println("The number of first array: " + firstArray.length);
 
-        /*
-        * PRACTICE: Print all the element of array with the comma as separator and end with the dot.
-        */
+        // PRACTICE: Print all the element of array with the comma as separator and end with the dot.
 
         System.out.println();
 
         System.out.println("C1: The list element of array by flattening a complex array to a simple array: ");
 
-        /*
-        Instead of looping through the array of array, we can flat it to a simple array
-        array = [[a, b, c], [d, e, f]];
-        ->
-        flattenArray = [a, b, c, d, e, f];
-        */
+        // Instead of looping through the array of array, we can flat it to a simple array
+        // array = [[a, b, c], [d, e, f]];
+        // ->
+        // flattenArray = [a, b, c, d, e, f];
 
         List<Integer> flattenArray = new ArrayList<>();
         for (int[] arr: joinedArray) {
@@ -73,12 +73,12 @@ public class MainArray {
 
         System.out.println();
 
-        /*
-        Looping through the Array of array, within each time, looping through the array to print the element
-        Using 2 variables due to using 2 loop:
-        - 1 variable to detect last array in array of array (countArray)
-        - 1 variable to detect last element in last array and print the dot to end (countElement)
-        */
+
+        // Looping through the Array of array, within each time, looping through the array to print the element
+        // Using 2 variables due to using 2 loop:
+        // - 1 variable to detect last array in array of array (countArray)
+        // - 1 variable to detect last element in last array and print the dot to end (countElement)
+
 
         System.out.println("C2: The list element of array by looping double array ");
         int countArray = 0;
@@ -116,6 +116,8 @@ public class MainArray {
             }
             countArray1 += 1;
         }
+
+        *** END: COMMENT OUT TO DO THE EXERCISES */
 
         /*
         PRACTICE:
@@ -168,6 +170,66 @@ public class MainArray {
             -> resultArray = [[0, 1], [2, 4], [5, 8], [9, 10]]
         */
 
+        /*
+        * Trước khi học mình cần làm rõ trước khái niệm Array và ArrayList trước ha e, xem mấy cái article này đễ rõ hơn à e:
+        * https://viettuts.vn/java-collection/su-khac-nhau-giua-array-voi-arraylist
+        * https://www.geeksforgeeks.org/array-vs-arraylist-in-java/
+        *
+        * int[] -> Array: perfomance nhanh nhưng rắc rối hơn, ít method support và khó thao tác
+        * ArrayList<Integer> -> ArrayList: perfomance ko cao nhưng có nhiều method hỗ trợ, dễ dùng
+        * nên mình thống nhất từ giờ về sau cần thao tác gì mình cứ chuyển hết về ArrayList rồi làm sẽ dễ hơn e ha
+        */
+
+        // int[] firstArray = new int[]{ 2, 4, 0, 6 };
+        List<Integer> firstArrayList = new ArrayList<Integer>();
+        firstArrayList.add(2);
+        firstArrayList.add(4);
+        firstArrayList.add(0);
+        firstArrayList.add(6);
+
+        // int[] secondArray = new int[]{ 2, 1, 0, 1 };
+        List<Integer> secondArrayList = new ArrayList<Integer>();
+        secondArrayList.add(2);
+        secondArrayList.add(1);
+        secondArrayList.add(0);
+        secondArrayList.add(1);
+
+        //int[] thirdArray = new int[]{ 0, 8, 0, 8, 2, 0, 1, 8 };
+        List<Integer> thirdArrayList = new ArrayList<Integer>();
+        thirdArrayList.add(0);
+        thirdArrayList.add(8);
+        thirdArrayList.add(0);
+        thirdArrayList.add(8);
+        thirdArrayList.add(2);
+        thirdArrayList.add(0);
+        thirdArrayList.add(1);
+        thirdArrayList.add(8);
+
+        List<List<Integer>> joinedArrayList = new ArrayList<>();
+        joinedArrayList.add(firstArrayList);
+        joinedArrayList.add(secondArrayList);
+        joinedArrayList.add(thirdArrayList);
+
+        // leave the empty type of the array as the simple way to define
+        // List<Integer> flattenedArray = new ArrayList<>(); and List<Integer> flattenedArray = new ArrayList<Integer>(); is the SAME
+        List<Integer> flattenedArray = new ArrayList<>();
+        for (List<Integer> arr: joinedArrayList) {
+            for (int arrElement : arr) {
+                flattenedArray.add(arrElement);
+            }
+        }
+
+        // Due to using the ArrayList instead of looping in the complex array of array, we can use the method addAll which support by arrayList to add the element
+        List<Integer> flattenedArray_1 = new ArrayList<>();
+        flattenedArray_1.addAll(firstArrayList);
+        flattenedArray_1.addAll(secondArrayList);
+        flattenedArray_1.addAll(thirdArrayList);
+
+        // E nhớ lưu ý nếu bị issue:
+        // 1 xem kĩ lại cheatsheet
+        // 2 debug thử
+        // 3 google xem ra không
+        // 4 e cứ hỏi a nha
     }
 
 }
