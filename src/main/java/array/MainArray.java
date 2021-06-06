20d33181407c415e0e6c5ae9b0ea3416937d6fcb
nhatnguyen-sm <nhat.minh.nguyen@mediastep.com>
2021-06-07 01:31:10 +0700

    Adding the answer for the first practice. Hope it help you.

--- src/main/java/array/MainArray.java
+++ src/main/java/array/MainArray.java
@@ -3,6 +3,7 @@ package array;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 
@@ -18,7 +19,7 @@ public class MainArray {
 
         int[] thirdArray = new int[]{ 0, 8, 0, 8, 2, 0, 1, 8 };
 
-        List<int []> joinedArray = new ArrayList<>();
+        ArrayList<int []> joinedArray = new ArrayList<>();
 
         joinedArray.add(firstArray);
         joinedArray.add(secondArray);
@@ -120,6 +121,68 @@ public class MainArray {
         *** END: COMMENT OUT TO DO THE EXERCISES */
 
         /*
+        * Trước khi học mình cần làm rõ trước khái niệm Array và ArrayList trước ha e, xem mấy cái article này đễ rõ hơn à e:
+        * https://viettuts.vn/java-collection/su-khac-nhau-giua-array-voi-arraylist
+        * https://www.geeksforgeeks.org/array-vs-arraylist-in-java/
+        *
+        * int[] -> Array: perfomance nhanh nhưng rắc rối hơn, ít method support và khó thao tác
+        * ArrayList<Integer> -> ArrayList: perfomance ko cao nhưng có nhiều method hỗ trợ, dễ dùng
+        * nên mình thống nhất từ giờ về sau cần thao tác gì mình cứ chuyển hết về ArrayList rồi làm sẽ dễ hơn e ha
+        */
+
+        // int[] firstArray = new int[]{ 2, 4, 0, 6 };
+        ArrayList<Integer> firstArrayList = new ArrayList<Integer>();
+        firstArrayList.add(2);
+        firstArrayList.add(4);
+        firstArrayList.add(0);
+        firstArrayList.add(6);
+
+        // int[] secondArray = new int[]{ 2, 1, 0, 1 };
+        ArrayList<Integer> secondArrayList = new ArrayList<Integer>();
+        secondArrayList.add(2);
+        secondArrayList.add(1);
+        secondArrayList.add(0);
+        secondArrayList.add(1);
+
+        //int[] thirdArray = new int[]{ 0, 8, 0, 8, 2, 0, 1, 8 };
+        ArrayList<Integer> thirdArrayList = new ArrayList<Integer>();
+        thirdArrayList.add(0);
+        thirdArrayList.add(8);
+        thirdArrayList.add(0);
+        thirdArrayList.add(8);
+        thirdArrayList.add(2);
+        thirdArrayList.add(0);
+        thirdArrayList.add(1);
+        thirdArrayList.add(8);
+
+        ArrayList<List<Integer>> joinedArrayList = new ArrayList<>();
+        joinedArrayList.add(firstArrayList);
+        joinedArrayList.add(secondArrayList);
+        joinedArrayList.add(thirdArrayList);
+
+        // leave the empty type of the array as the simple way to define
+        // List<Integer> flattenedArray = new ArrayList<>(); and List<Integer> flattenedArray = new ArrayList<Integer>(); is the SAME
+        ArrayList<Integer> flattenedArray = new ArrayList<>();
+        for (List<Integer> arr: joinedArrayList) {
+            for (int arrElement : arr) {
+                flattenedArray.add(arrElement);
+            }
+        }
+
+        // Due to using the ArrayList instead of looping in the complex array of array, we can use the method addAll which support by arrayList to add the element
+        ArrayList<Integer> flattenedArray_1 = new ArrayList<>();
+        flattenedArray_1.addAll(firstArrayList);
+        flattenedArray_1.addAll(secondArrayList);
+        flattenedArray_1.addAll(thirdArrayList);
+
+        // E nhớ lưu ý nếu bị issue:
+        // 1 xem kĩ lại cheatsheet
+        // 2 debug thử
+        // 3 google xem ra không
+        // 4 e cứ hỏi a nha
+
+        // QUESTIONS:
+        /*
         PRACTICE:
         Hope you have time to take care on this practice at weekend...
         Please take a look on the cheat sheet before doing them... I can not do something difference...
@@ -141,6 +204,7 @@ public class MainArray {
             int[] newArray = [1, 7, 9, 1, 7, 2, 3, 4, 2];
             -> int[] result = [9, 7, 4, 3, 2, 1];
 
+        From this question please using the input array as the result of question 2
         3. Get the total of the array of practice 2
         Ex:
             int[] newArray = [1, 2, 3];
@@ -170,66 +234,104 @@ public class MainArray {
             -> resultArray = [[0, 1], [2, 4], [5, 8], [9, 10]]
         */
 
-        /*
-        * Trước khi học mình cần làm rõ trước khái niệm Array và ArrayList trước ha e, xem mấy cái article này đễ rõ hơn à e:
-        * https://viettuts.vn/java-collection/su-khac-nhau-giua-array-voi-arraylist
-        * https://www.geeksforgeeks.org/array-vs-arraylist-in-java/
-        *
-        * int[] -> Array: perfomance nhanh nhưng rắc rối hơn, ít method support và khó thao tác
-        * ArrayList<Integer> -> ArrayList: perfomance ko cao nhưng có nhiều method hỗ trợ, dễ dùng
-        * nên mình thống nhất từ giờ về sau cần thao tác gì mình cứ chuyển hết về ArrayList rồi làm sẽ dễ hơn e ha
-        */
+        // Hi vọng là e đã giải được phần nào bài tập rồi à e, nếu có chỗ nào chưa rõ e chịu khó xem thử phần này e nha
+        // ANSWERS:
 
-        // int[] firstArray = new int[]{ 2, 4, 0, 6 };
-        List<Integer> firstArrayList = new ArrayList<Integer>();
-        firstArrayList.add(2);
-        firstArrayList.add(4);
-        firstArrayList.add(0);
-        firstArrayList.add(6);
+        // 1
+        // dùng phương thức clone() để tạo 1 đối đượng mới từ arrayList đã cho ở đề bài để không làm thay đổi mảng đã cho ban đầu
+        System.out.println("Câu 1: ");
+        ArrayList<Integer> cau1 = (ArrayList<Integer>) flattenedArray_1.clone();
+        Collections.sort(cau1);
+        printArray(cau1);
+        System.out.println();
 
-        // int[] secondArray = new int[]{ 2, 1, 0, 1 };
-        List<Integer> secondArrayList = new ArrayList<Integer>();
-        secondArrayList.add(2);
-        secondArrayList.add(1);
-        secondArrayList.add(0);
-        secondArrayList.add(1);
-
-        //int[] thirdArray = new int[]{ 0, 8, 0, 8, 2, 0, 1, 8 };
-        List<Integer> thirdArrayList = new ArrayList<Integer>();
-        thirdArrayList.add(0);
-        thirdArrayList.add(8);
-        thirdArrayList.add(0);
-        thirdArrayList.add(8);
-        thirdArrayList.add(2);
-        thirdArrayList.add(0);
-        thirdArrayList.add(1);
-        thirdArrayList.add(8);
-
-        List<List<Integer>> joinedArrayList = new ArrayList<>();
-        joinedArrayList.add(firstArrayList);
-        joinedArrayList.add(secondArrayList);
-        joinedArrayList.add(thirdArrayList);
-
-        // leave the empty type of the array as the simple way to define
-        // List<Integer> flattenedArray = new ArrayList<>(); and List<Integer> flattenedArray = new ArrayList<Integer>(); is the SAME
-        List<Integer> flattenedArray = new ArrayList<>();
-        for (List<Integer> arr: joinedArrayList) {
-            for (int arrElement : arr) {
-                flattenedArray.add(arrElement);
+        // 2
+        ArrayList<Integer> cau2 = (ArrayList<Integer>) flattenedArray_1.clone();
+        // tạo 1 mảng rỗng để chứa những phần tử không trùng nhau,
+        // loop qua mảng đã cho kiểm tra phần tử nếu chưa tồn tại trong mảng thì thêm vào mảng kết quả, nếu có rồi thì bỏ qua
+        System.out.println("Câu 2: ");
+        ArrayList<Integer> uniqueArrayList = new ArrayList<>();
+        for (int i: cau2) {
+            if (uniqueArrayList.contains(i) == false) {
+                uniqueArrayList.add(i);
             }
         }
+        // sắp xếp mảng kết quả theo thứ tự giảm dần
+        Collections.sort(uniqueArrayList, Collections.reverseOrder());
+        printArray(uniqueArrayList);
+        System.out.println();
 
-        // Due to using the ArrayList instead of looping in the complex array of array, we can use the method addAll which support by arrayList to add the element
-        List<Integer> flattenedArray_1 = new ArrayList<>();
-        flattenedArray_1.addAll(firstArrayList);
-        flattenedArray_1.addAll(secondArrayList);
-        flattenedArray_1.addAll(thirdArrayList);
+        // 3
+        System.out.println("Câu 3: ");
+        int sum = 0;
+        ArrayList<Integer> cau3 = (ArrayList<Integer>) uniqueArrayList.clone();
+        for (int i: cau3) {
+            sum += i;
+        }
+        System.out.println(sum);
+        System.out.println();
 
-        // E nhớ lưu ý nếu bị issue:
-        // 1 xem kĩ lại cheatsheet
-        // 2 debug thử
-        // 3 google xem ra không
-        // 4 e cứ hỏi a nha
+        // 4
+        System.out.println("Câu 4: ");
+        ArrayList<Integer> cau4 = (ArrayList<Integer>) uniqueArrayList.clone();
+        cau4.add(2406);
+        printArray(cau4);
+        System.out.println();
+
+        // 5
+        System.out.println("Câu 5: ");
+        ArrayList<Integer> cau5 = (ArrayList<Integer>) uniqueArrayList.clone();
+        cau5.add(0, 2406);
+        printArray(cau5);
+        System.out.println();
+
+        // 6
+        System.out.println("Câu 6: ");
+        ArrayList<Integer> cau6 = (ArrayList<Integer>) uniqueArrayList.clone();
+        ArrayList<Integer> divided13 = new ArrayList<>();
+        for (int i = 13; i < 100; i+= 13) {
+            divided13.add(i);
+            if (divided13.size() == 3) {
+                break;
+            }
+        }
+        int middleIndex = cau6.size() / 2;
+        cau6.addAll(middleIndex, divided13);
+        printArray(cau6);
+        System.out.println();
+
+        // 7
+        System.out.println("Câu 7: ");
+        ArrayList<Integer> cau7 = (ArrayList<Integer>) flattenedArray_1.clone();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        ArrayList<Integer> tempArray = new ArrayList<>();
+        for (int i = 0; i < cau7.size(); i++) {
+            tempArray.add(cau7.get(i));
+            if (tempArray.size() == 2 || i == cau7.size() - 1) {
+                result.add((ArrayList<Integer>) tempArray.clone());
+                tempArray.clear();
+            }
+        }
+        System.out.print("[");
+        for (ArrayList<Integer> childArray :result) {
+            System.out.print(" [");
+            for (int i = 0; i < childArray.size(); i++) {
+                System.out.print(childArray.get(i));
+                if (i != childArray.size() - 1) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.print("] ");
+        }
+        System.out.print("]");
+        System.out.println();
+    }
+
+    private static void printArray(ArrayList<Integer> arrayList) {
+        for (int i: arrayList) {
+            System.out.print(i  + " ");
+        }
+        System.out.println();
     }
 
 }
